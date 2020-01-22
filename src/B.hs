ee949cc58465d56c9a70199bb04db035c6cffb40
Yiyun Liu <liuyiyun@terpmail.umd.edu>
2020-01-22 10:23:06 -0600

    fix elaborateExpr

--- src/B.hs
+++ src/B.hs
@@ -3,7 +3,17 @@ module B where
 
 import Data.Semigroup
 
+data Thing = Thing
+
 semop :: Sum Int -> Sum Int -> Sum Int
 semop x y = (<>) x y
 
 main = print "Hello, World!"
+
+class YSem a where
+  ymappend :: a -> a -> a
+  lawAssoc :: a -> a -> a -> ()
+
+instance YSem Int where
+  ymappend = (+)
+  lawAssoc _ _ _ = ()
--- src/Main.hs
+++ src/Main.hs
@@ -2,8 +2,10 @@ module Main where
 
 import GHC
 import CoreSyn
+import Inst
 import Desugar
 import TcRnMonad
+import TcHsSyn
 import RnExpr
 import GhcMonad
 import TcSimplify
@@ -40,18 +42,18 @@ example =
         setTargets [target]
         load LoadAllTargets
         let modBName = mkModuleName "B"
-        modSum <- getModSummary $ modBName
-        p <- parseModule modSum
-        t <- typecheckModule p
-        d <- desugarModule t
-        l <- loadModule d
+        modSum <- getModSummary modBName
+        -- p <- parseModule modSum
+        -- t <- typecheckModule p
+        -- d <- desugarModule t
+        -- l <- loadModule d
         setContext [IIModule $ ms_mod_name modSum]
-        myE <- elaborateExpr "(\\ x y -> x  <> y) :: Sum Int -> Sum Int -> Sum Int"
-        n <- getNamesInScope
-        c <- return $ coreModule d
+        myE <- elaborateExpr  "(\\ x y ->  Sum x <> Sum y) :: Num a => a -> a -> Sum a "
+        -- n <- getNamesInScope
+        -- c <- return $ coreModule d
  
-        g <- getModuleGraph
-        mapM showModule (mgModSummaries g)
+        -- g <- getModuleGraph
+        -- mapM showModule (mgModSummaries g)
 
         return $ myE
 
@@ -79,12 +81,24 @@ elabRnExpr hsc_env rdr_expr
                                  uniq <- newUnique ;
                                  let { fresh_it  = itName uniq (getLoc rdr_expr)
                                      ; orig = lexprCtOrigin rn_expr } ;
-                                 pp <- captureTopConstraints $
-                                   pushTcLevelM          $
-                                   tcInferSigma rn_expr ;
-                                 pure (fst pp)
+                                 pp <- pushLevelAndCaptureConstraints $
+                                   do {(_tc_expr, expr_ty) <- tcInferSigma rn_expr;
+                                       expr_ty' <- snd <$> deeplyInstantiate orig expr_ty;
+                                       pure (_tc_expr, expr_ty')} ;
+
+                                 (tclvl, lie, (tc_expr, res_ty)) <- pure pp;
+                                 (qtvs, dicts, _, residual, _)
+                                     <- simplifyInfer tclvl NoRestrictions
+                                                      []    {- No sig vars -}
+                                                      [(fresh_it, res_ty)]
+                                                      lie ;
+
+                                 -- Ignore the dictionary bindings
+                                 _ <- simplifyInteractive residual ;
+
+                                 zonkTopLExpr tc_expr
                                  })
      case maybe_tc_expr of
        Nothing -> pure (msgs, Nothing)
-       Just tc_expr -> deSugarExpr hsc_env (fst . fst $ tc_expr)
+       Just tc_expr -> deSugarExpr hsc_env tc_expr
 
